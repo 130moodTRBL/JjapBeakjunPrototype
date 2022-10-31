dc9197e7d93dd8c61aba136c9854790a3409aaf4
130moodTRBL <sonic6679@naver.com>
2022-11-01 00:21:08 +0900

    welcome

---
+++ src/test/java/com/example/jjapjun/JjapjunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjapjun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjapjunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
