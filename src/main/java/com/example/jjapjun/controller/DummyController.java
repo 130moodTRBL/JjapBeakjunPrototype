a35804b935e1f841ebd3c9f9334e003e4bcefb32
130moodTRBL <sonic6679@naver.com>
2022-11-02 00:47:45 +0900

    create, update테스트

--- src/main/java/com/example/jjapjun/controller/DummyController.java
+++ src/main/java/com/example/jjapjun/controller/DummyController.java
@@ -5,9 +5,15 @@ import com.example.jjapjun.repository.UserRepository;
 import com.example.jjapjun.role.UserRole;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 @Slf4j
 @RestController
@@ -33,7 +39,39 @@ public class DummyController {
     }
 
     @GetMapping("/dummy/user/{id}")
-    public User detail() {
+    public User detail(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("없는 유저 id:" + id);
+        });
 
+        return user;
+    }
+
+    @GetMapping("/dummy/users")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<User> pageUser = userRepository.findAll(pageable);
+        List<User> users = pageUser.getContent();
+        return users;
+    }
+
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User requestUser) {
+        log.warn("id={}", id);
+        log.warn("password={}", requestUser.getPassword());
+        log.warn("email={}", requestUser.getEmail());
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정 실패");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+        //userRepository.save(user);
+
+        return null;
     }
 }
